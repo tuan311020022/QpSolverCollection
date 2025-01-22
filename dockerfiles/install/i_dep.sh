9b0ae4126d3222c4ed795dc9e0bb047e8f007506
tuan311020022 <nguyen.minhtuan@hachi-x.com>
2025-01-22 08:12:39 +0700

    👍 remove unused code

--- dockerfiles/install/i_dep.sh
+++ dockerfiles/install/i_dep.sh
@@ -18,44 +18,12 @@ basic_dep="git \
            fluxbox \
            ffmpeg"
 
-# ROS dependencies
-ros_dep="ros-noetic-robot-state-publisher \
-         ros-noetic-urdf \
-         ros-noetic-tf \
-         ros-noetic-xacro \
-         ros-noetic-controller-manager \
-         ros-noetic-controller-manager-* \
-         ros-noetic-effort-controllers \
-         ros-noetic-joint-state-controller \
-         ros-noetic-rviz \
-         ros-noetic-geographic-msgs \
-         ros-noetic-ros-controllers \
-         ros-noetic-navigation \
-         ros-noetic-gazebo-ros \
-         ros-noetic-gazebo-ros-* \
-         ros-noetic-moveit \
-         ros-noetic-moveit-visual-tools \
-         ros-noetic-ros-control-boilerplate \
-         ros-noetic-rosbridge-server
-         "
-
-python_dep="open3d \
-            numpy==1.24.4"
-
 # Install basic dependencies
 apt-get install -y $basic_dep
 
 # Update and upgrade the system again
 apt-get update
 apt-get upgrade -y
-
-# Install ROS dependencies
-DEBIAN_FRONTEND=noninteractive apt-get install -y $ros_dep
-
-pip install $python_dep
-
-# Set the GAZEBO_MODEL_PATH
-# export GAZEBO_MODEL_PATH=/root/catkin_ws/src/aist_launch/models:$GAZEBO_MODEL_PATH
 
 mkdir /root/catkin_ws
 cd /root/catkin_ws
